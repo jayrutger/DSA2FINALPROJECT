b77e4655d3c35d134b0e3fa77d3a5e65730c0a20
jayrutger <jar110@students.uwf.edu>
2020-04-11 20:16:15 -0500

    Part 1a working for real

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -47,36 +47,52 @@ void MonteCarlo::PrintBatches()
 {
 //	std::string fileName = "ds";
 	
-	bool isGood;
+	bool isGoodBatch;
+	bool isGoodChip;
 		
 
 	for(int i=0; i<this->numOfBatch; i++)
 	{
+
 		std::string fileName = "ds";
 		fileName += std::to_string(i+1);
 		fileName += ".txt";
 		std::cout << fileName << std::endl;
 
+		isGoodBatch = true;
+
+		int num1 = std::rand() % 100;
+		if(num1 > 0 && num1 <= percentBadBatches)
+		{
+			isGoodBatch = false;
+		}	
 		std::ofstream myFile;
 		myFile.open(fileName);
+
 		for(int j=0;j<this->numItems;j++)
 		{
+			isGoodChip = true;
 
-			isGood = true;
-
-			int num = std::rand() % 100;
-			if(num > 0 && num < percentBadBatches)
-			{
-				isGood = false;
-			}	
-
-			if(isGood == true)
+			if(isGoodBatch == true)
 			{
 				myFile << "g" << std::endl;
 			}
 			else
 			{
-				myFile << "b" << std::endl;
+				int num2 = std::rand() % 100;
+				if(num2 > 0 && num2 <= percentBadness)
+				{
+					isGoodChip = false;
+				}	
+	
+				if(isGoodChip == true)
+				{
+					myFile << "g" << std::endl;
+				}
+				else
+				{
+					myFile << "b" << std::endl;
+				}
 			}
 		}
 		myFile.close();
